96f853142cc0adf479922b14c8fc5f84a4ee6d87
D3Portillo <d3portillo@gmail.com>
2022-07-31 06:55:13 +0200

    chores(tailwind.config.js): add content matcher for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type { import('tailwindcss').Config } */
 
 module.exports = {
-  content: ["./pages/*.js", "./pages/**/*.js"],
+  content: ["./pages/*.js", "./pages/**/*.js", "./components/**/*.js"],
   theme: {
     extend: {},
   },
