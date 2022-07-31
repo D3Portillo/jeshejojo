333e1f606a7923fc76b71944ff267145999de5fb
D3Portillo <d3portillo@gmail.com>
2022-07-31 07:10:30 +0200

    enh(Footer/): add page footer :p

---
+++ components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import ExternalLink from "@/components/ExternalLink"
+
+function Footer() {
+  return (
+    <footer className="text-sm p-4 text-center mt-20">
+      Jeshejojos. A pet project to practice Solidity — Created by
+      <ExternalLink href="https://twitter.com/d3portillo">
+        @d3portillo
+      </ExternalLink>
+    </footer>
+  )
+}
+
+export default Footer
