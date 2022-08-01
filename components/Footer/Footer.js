ed1eebc0be2c4010cd5d16e9be98e8382fcf851a
D3Portillo <d3portillo@gmail.com>
2022-08-01 22:40:36 +0200

    fix(Footer.js): add bg-white to ExternalLink.className

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="text-sm p-4 text-center mt-20">
       Jeshejojos. A pet project to practice Solidity — Created by
-      <ExternalLink href="https://twitter.com/d3portillo">
+      <ExternalLink className="bg-white" href="https://twitter.com/d3portillo">
         @d3portillo
       </ExternalLink>
     </footer>
