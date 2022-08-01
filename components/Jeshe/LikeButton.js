55d5a3adf9178a702d90d2ec7c37657c638a80f9
D3Portillo <d3portillo@gmail.com>
2022-08-01 22:41:19 +0200

    enh(LikButton.js): micro intereaction now are similar to ExternalLink : )

--- components/Jeshe/LikeButton.js
+++ components/Jeshe/LikeButton.js
@@ -1,12 +1,14 @@
 import classnames from "@/lib/classnames"
 
-function LikeButton({ children = null, onClick, isActive = false }) {
+function LikeButton({ isMock, children = null, onClick, isActive = false }) {
   return (
     <button
       onClick={onClick}
+      disabled={isMock}
       className={classnames(
         "hover:bg-zinc-100 py-1 px-2 rounded-lg active:scale-105 ring-slate-200 focus:ring-2",
-        isActive && "bg-zinc-100"
+        isActive && "bg-zinc-100",
+        isMock && "opacity-75 animate-pulse"
       )}
     >
       {children}
