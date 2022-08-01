46cc127d6cc3aeb21441f5f119b5702618064f27
D3Portillo <d3portillo@gmail.com>
2022-08-01 22:37:31 +0200

    enh(ExternalLink): component now expects className prop

--- components/ExternalLink/ExternalLink.js
+++ components/ExternalLink/ExternalLink.js
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import { FiArrowUpRight } from "react-icons/fi"
+import classnames from "@/lib/classnames"
 
-function ExternalLink({ href = "", children = null }) {
+function ExternalLink({ href = "", children = null, className }) {
   return (
     <Link href={href}>
       <a
-        className="text-sm group inline-flex space-x-1 items-center hover:bg-zinc-100 py-1 px-2 rounded-lg"
+        className={classnames(
+          "text-sm group inline-flex space-x-1 items-center hover:bg-zinc-100 active:scale-105 py-1 px-2 rounded-lg",
+          "focus:ring-2 ring-slate-100",
+          className
+        )}
         target="_blank"
       >
         <span>{children}</span>
