0778438ef9fb68840d032dcbf31753c947f6c92b
D3Portillo <d3portillo@gmail.com>
2022-08-01 22:36:44 +0200

    fix(services/setLikes.ts): do not validate if item-id is defined [Happens in BE]

--- lib/services/setLikes.ts
+++ lib/services/setLikes.ts
@@ -3,8 +3,8 @@ import fetcher from "./fetcher"
 type Body = { id: string | number; address: string; type: string }
 
 function setLikes(body: Body) {
-  const { address, id } = body
-  if (!address || !id) return Promise.resolve()
+  const { address } = body
+  if (!address) return Promise.resolve()
   return fetcher<Body, any>("/likes/set", {
     body,
     query: {},
