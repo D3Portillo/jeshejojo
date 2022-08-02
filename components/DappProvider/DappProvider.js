e546b3c29d740cf4550fb35da7d2db1633f9c345
D3Portillo <d3portillo@gmail.com>
2022-08-02 06:26:25 +0200

    refactor(DappProvider.js): replace IS_DEV with #isDevEnv 4e9d41b968a4e91954b3c7fe092a0bc5aabd443a)

--- components/DappProvider/DappProvider.js
+++ components/DappProvider/DappProvider.js
@@ -3,10 +3,11 @@ import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi"
 import { alchemyProvider } from "wagmi/providers/alchemy"
 import { publicProvider } from "wagmi/providers/public"
+import { isDevEnv } from "@/lib/helpers"
 
-const IS_DEV = process.env.NODE_ENV === "development"
+const IS_DEV = isDevEnv()
 export const { chains, provider, webSocketProvider } = configureChains(
-  [chain.goerli, ...(IS_DEV ? [chain.hardhat] : [chain.mainnet])],
+  [chain.goerli, ...(IS_DEV ? [chain.hardhat] : [/** chain.mainnet, chain.polygon */ ])],
   [
     alchemyProvider({ alchemyId: process.env.AlCHEMY_API_KEY }),
     publicProvider(),
