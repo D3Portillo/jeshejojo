ef29afe6cae1c8449a668438ab63344b69878e62
D3Portillo <d3portillo@gmail.com>
2022-08-04 20:47:24 +0200

    enh(package.json,_app.js): add Toaster. react-hot-toast dep

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import DappProvider from "@/components/DappProvider"
+import { Toaster } from "react-hot-toast"
 import "@/assets/styles.css"
 
 function DApp({ Component, pageProps }) {
   return (
     <DappProvider>
+      <Toaster position="top-center" reverseOrder={false} />
       <Component {...pageProps} />
     </DappProvider>
   )
