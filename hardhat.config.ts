7541a619df531996598afce5b15b9fc3ed816e95
D3Portillo <d3portillo@gmail.com>
2022-08-10 01:32:21 +0200

    enh: add hardhat-chai-matchers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-chai-matchers";
 import "dotenv/config"
 import "hardhat-deploy"
 
