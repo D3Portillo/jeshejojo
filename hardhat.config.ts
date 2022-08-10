3fbcc95fd8b7463054e6d3d2df2fc30e3e54c428
D3Portillo <d3portillo@gmail.com>
2022-08-10 22:58:39 +0200

    enh(hardhat.config): add hardhat-etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
 import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-etherscan";
+
 import "dotenv/config"
 import "hardhat-deploy"
 
 const GOERLI_ALCHEMY_URL = process.env.GOERLI_ALCHEMY_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY!
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
@@ -25,6 +28,9 @@ const config: HardhatUserConfig = {
       default: 0,
     },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY
+  }
 }
 
 export default config
