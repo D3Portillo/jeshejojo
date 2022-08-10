0c262fd8c4361b5b5f96f8f0b2b78305d4824661
D3Portillo <d3portillo@gmail.com>
2022-08-10 23:00:51 +0200

    chores(MeinJokes.sol): add NatSpec reference comments

--- contracts/MeinJokes.sol
+++ contracts/MeinJokes.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// NOTE: I want to enhance the way colors are stored.
+// Currently a HEX string is stored. Suggestions are welcomed :)
 struct Jeshe {
     bytes6 bgColor;
     bytes6 textColor;
@@ -8,20 +10,31 @@ struct Jeshe {
     string content;
 }
 
+/// @title A Simple yet useful starter contract :)
+/// @author Denny Portillo
+/// @notice This implementation is used as a learning tool. Do not use in real-apps
 contract MeinJokes {
     uint64 nextItemIdx;
     mapping(uint64 => Jeshe) itemsById;
 
+    /// @notice Log items by listed by owner along with it's item_id
+    /// @dev This indexed owner-item_id is usefull along with getItemsById fn
     event ListedItem(address indexed owner, uint64 indexed item_id);
 
     function totalItems() external view returns (uint64) {
         return nextItemIdx;
     }
 
+    /// @notice Get information about an item by providing it's id
     function getItemById(uint64 _itemId) external view returns (Jeshe memory) {
         return itemsById[_itemId];
     }
 
+    /// @notice List/Create a Jeshejojo
+    /// @dev Emits #ListedItem
+    /// @param _content The message you want to store
+    /// @param _bgColor The background of your post. Just as facebook/instagram stories :p
+    /// @param _textColor Set a custom font-color for your post
     function listItem(
         string calldata _content,
         bytes6 _bgColor,
