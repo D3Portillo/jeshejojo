c17e44477d0d610a2ed53b102a49e8a10192f923
D3Portillo <d3portillo@gmail.com>
2022-08-19 17:16:05 +0200

    feat(pages/id): show item by id

--- pages/id/[id].js
+++ pages/id/[id].js
@@ -1,10 +1,17 @@
+import { useRouter } from "next/router"
+
+import { FeedContainer } from "@/components/Feed/Feed"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import Jeshe from "@/components/Jeshe"
 
-export default function Id() {
+export default function SingleItemId() {
+  const router = useRouter()
+  const { id } = router.query
   return (
     <div className="min-h-screen">
       <Header />
+      <FeedContainer>{id && <Jeshe id={id} />}</FeedContainer>
       <Footer />
     </div>
   )
